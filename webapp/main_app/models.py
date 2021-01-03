6f85e2e2776b87dfb604a05ead5b6bfd4af5f884
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2021-01-03 20:29:41 +0300

    Добавил возможность ссылаться на разные таблицы в проекте

--- webapp/main_app/models.py
+++ webapp/main_app/models.py
@@ -1,9 +1,21 @@
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
 from webapp.model import db
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200))
+    text = db.Column(db.Text, nullable=False)
     Done = db.Column(db.Boolean)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+    
+    user = relationship('User', backref='todo')
     
     def __repr__(self):
         return '<Id {}>'.format(self.id)
