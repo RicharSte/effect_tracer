33b46530819626c95d9b16abc05be3b1fac4b6d2
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2021-01-05 20:50:38 +0300

    Теперь каждый видит только свои таски

--- webapp/main_app/views.py
+++ webapp/main_app/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint ,render_template, flash, redirect, request, url_for
+from flask import Blueprint, render_template, flash, redirect, request, url_for
+from flask_login import current_user
 
 from webapp import db
 from webapp.main_app.forms import TodoForm
@@ -10,15 +11,15 @@ blurptint = Blueprint('main_app', __name__, url_prefix='/main_app')
 @blurptint.route('/')
 def index():
     todo_form = TodoForm()
-    incomplete = Todo.query.filter_by(Done=False).all()
-    complete = Todo.query.filter_by(Done=True).all()
+    incomplete = Todo.query.filter_by(Done=False, user_id =current_user.id).all()
+    complete = Todo.query.filter_by(Done=True, user_id =current_user.id).all()
     return render_template('main_app/index.html', incomplete=incomplete, complete=complete, form=todo_form)
 
 
 @blurptint.route('/add', methods=['POST'])
 def add():
     todo_form = TodoForm()
-    todo = Todo(text=request.form['task'], Done=False)
+    todo = Todo(text=request.form['task'], user_id =current_user.id, Done=False)
     db.session.add(todo)
     db.session.commit()
     
